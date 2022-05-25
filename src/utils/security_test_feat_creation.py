33d23f6380f8746a1acdceb081c819b1687df261
Son Chau <soncqb@gmail.com>
2022-05-24 23:47:45 -0700

    fix: retain the column order

--- src/utils/security_test_feat_creation.py
+++ src/utils/security_test_feat_creation.py
@@ -21,11 +21,11 @@ def security_test_feat_creation(df):
     # Define column types
     categorical_features = ["security_test_category"]
     ordinal_features = ["security_test_result"]
-    passthrough_features = list(
-        set(df.columns)
-        - set(categorical_features)
-        - set(ordinal_features)
-    )
+    # passthrough_features is the list of columns in df minus the categorical and ordinal features
+    passthrough_features = [
+        feature for feature in df.columns 
+        if feature not in categorical_features + ordinal_features
+    ]
     # Define levels for ordinal encoder
     test_result_levels = [
         0.0,
