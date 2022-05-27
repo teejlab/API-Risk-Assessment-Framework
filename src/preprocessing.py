bc7770125cd2cd4d0a5d4c64990daf53d8b5106c
Son Chau <soncqb@gmail.com>
2022-05-26 23:00:53 -0700

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,13 +1,14 @@
 """Reads train csv data from path, preprocess the data, and save the preprocessed data to path.
-Usage: preprocessing.py --input_path=<input_path> --input_path_country=<input_path_country> --output_path=<output_path>
+Usage: preprocessing.py --endpoint_path=<endpoint_path> --country_path=<country_path> --risk_rules_path=<risk_rules_path> --output_path=<output_path>
  
 Options:
---input_path=<input_path>                    Path to input data
---input_path_country=<input_path_country>    Path to input country metric data
---output_path=<output_path>                  Path for preprocessed file to be saved
+--endpoint_path=<endpoint_path>         Path to input data
+--country_path=<country_path>           Path to input country metric data
+--risk_rules_path=<risk_rules_path>     Path to risk rules
+--output_path=<output_path>             Path for preprocessed file to be saved
 
 Example:
-python src/preprocessing.py --input_path=data/raw/RiskClassification_Data_Endpoints_V4_Shared1.xlsx --input_path_country=data/raw/nri_2021_dataset.xlsx --output_path=data/processed/
+python src/preprocessing.py --endpoint_path=data/raw/RiskClassification_Data_Endpoints_V4_Shared1.xlsx --country_path=data/raw/nri_2021_dataset.xlsx --risk_rules_path=data/raw/RiskClassification_Data_Endpoints_V2.xlsx --output_path=data/processed/
 """
 
 from docopt import docopt
@@ -15,20 +16,21 @@ from sklearn.model_selection import train_test_split
 from utils.country_n_cat_featuring import add_country_and_cat_feats
 from utils.metadata_extraction import extract_metadata
 from utils.pii_extraction import pii_extraction
+from utils.risk_labelling import create_risk_label
 from utils.security_test_feat_creation import security_test_feat_creation
 import pandas as pd
 from pathlib import Path
 import swifter
 
 opt = docopt(__doc__)
-def main(input_path, input_path_country, output_path):
+def main(endpoint_path, country_path, risk_rules_path, output_path):
     path = Path(output_path)
     path.mkdir(parents=True, exist_ok=True)
 
     #############
     # READ DATA #
     #############
-    df = pd.read_excel(input_path, "Core_Endpoint", usecols="A:S")
+    df = pd.read_excel(endpoint_path, "Core_Endpoint", usecols="A:S")
     df.rename(
         columns={
             "security_test_result (FALSE=Passed; TRUE=Failed)": "security_test_result"
@@ -47,10 +49,10 @@ def main(input_path, input_path_country, output_path):
     path_to_pii = output_path + "/df_pii.xlsx"
     pii_path = Path(path_to_pii)
     if not pii_path.is_file():    # delete the file if you want to run it again
-        print("Extracting PII and FII features...")
-        df["is_pii"] = df["sample_response"].swifter(pii_extraction, 
+        print("Extracting PII and FII features. This will take a while...")
+        df["is_pii"] = df["sample_response"].swifter.apply(pii_extraction, 
             args=("pii", 0.5,)).astype(bool)
-        df["is_fii"] = df["sample_response"].swifter(pii_extraction, 
+        df["is_fii"] = df["sample_response"].swifter.apply(pii_extraction,
             args=("fii", 0.5,)).astype(bool)
         # save df with pii and fii
         df.to_excel(output_path + "/df_pii.xlsx", index=False)
@@ -61,29 +63,34 @@ def main(input_path, input_path_country, output_path):
     #############################################
     # ADD COUNTRY SCORE AND CATEGORIES FEATURES #
     #############################################
-    df = add_country_and_cat_feats(df, input_path_country)
+    print(f'Adding country score and OHE categories features...')
+    df = add_country_and_cat_feats(df, country_path)
     df.to_excel(output_path + "/df_country_score.xlsx", index=False)
 
 
     ##############################
     # ADD SECURITY TEST FEATURES #
     ##############################
+    print(f'Adding security test features...')
     df = security_test_feat_creation(df)
     df.to_excel(output_path + "/df_security.xlsx", index=False)
 
     #########################
     # ADD METADATA FEATURES #
     #########################
+    print(f'Adding metadata features...')
     df = extract_metadata(df)
     df.to_excel(output_path + "/df_metadata.xlsx", index=False)
 
     ###################
     # ADD RISK LABELS #
     ###################
-    # read risk_labeled.xlsx
-    df_risk_labeled = pd.read_excel("data/processed/risk_labeled.xlsx")
-    # merge df and df_risk_labeled
-    df = df.merge(df_risk_labeled, how="left", on="api_endpoint_id")
+    # # read risk_labeled.xlsx
+    # df_risk_labeled = pd.read_excel("data/processed/risk_labeled.xlsx")
+    # # merge df and df_risk_labeled
+    # df = df.merge(df_risk_labeled, how="left", on="api_endpoint_id")
+    print(f'Adding risk labels...')
+    df = create_risk_label(df, risk_rules_path, path_to_pii)
     df.to_excel(output_path + "/df_full.xlsx", index=False)
 
     # make a copy of orignial_df, select only api_endpoint_id and security_test_category
@@ -101,4 +108,5 @@ def main(input_path, input_path_country, output_path):
     test_df.to_csv(output_path + "/test.csv", index=False)
 
 if __name__ == "__main__":
-    main(opt["--input_path"], opt["--input_path_country"], opt["--output_path"])
+    main(opt["--endpoint_path"], opt["--country_path"],
+         opt["--risk_rules_path"], opt["--output_path"])
